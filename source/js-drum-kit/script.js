ef3de706c56cc651f46ed033587d24b69bfe9347
Alexander Babchenko <babtshe@yandex.com>
2019-10-07 20:54:30 +0300

    add js-drum-kit

--- source/js-drum-kit/script.js
+++ source/js-drum-kit/script.js
@@ -1,4 +1,28 @@
 'use strict';
 (function () {
+  const keysElements = document.querySelectorAll('.keys .key');
+  const audioElements = document.querySelectorAll('audio');
+
+  const onTransitionEnd = (evt) => {
+    evt.target.classList.remove('playing');
+    evt.target.removeEventListener('transitionend', onTransitionEnd);
+  };
+
+  const onKeyDown = (evt) => {
+    evt.preventDefault();
+    const targetKey = [...keysElements].find(item => +item.dataset.key === evt.keyCode);
+    const targetAudio = [...audioElements].find(item => +item.dataset.key === evt.keyCode);
+    if (!targetAudio || !targetKey) {
+      return;
+    }
+
+    targetAudio.currentTime = 0;
+    targetAudio.play()
+
+    targetKey.classList.add('playing');
+    targetKey.addEventListener('transitionend', onTransitionEnd);
+  };
+
+  window.addEventListener('keydown', onKeyDown);
 
 })();
