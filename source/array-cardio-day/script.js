68704f28d98ed375fa8c3485019b4634e2a5156b
Alexander Babchenko <babtshe@yandex.com>
2019-10-14 16:36:46 +0300

    add more arrays cardio

--- source/array-cardio-day/script.js
+++ source/array-cardio-day/script.js
@@ -65,4 +65,42 @@
   }, {});
   console.log(instancesOfData);
 
+  // ## Array Cardio Day 2
+
+  const newPeople = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+
+  // Some and Every Checks
+  // Array.prototype.some() // is at least one person 19 or older?
+  console.log(`${newPeople
+    .some(i=>(new Date().getFullYear() - i.year) >= 19)?'at least one person':'no one'} is 19 or older`);
+  // Array.prototype.every() // is everyone 19 or older?
+  console.log(`${newPeople
+    .every(i=>(new Date().getFullYear() - i.year) >= 19)?'':'not '}everyone is 19 or older`);
+
+  // Array.prototype.find()
+  // Find is like filter, but instead returns just the one you are looking for
+  // find the comment with the ID of 823423
+  console.log(`comment with the ID 823423`, comments.find(a=>a.id === 823423));
+
+  // Array.prototype.findIndex()
+  console.log(`index of comment with the ID 823423 =`, comments.findIndex(a=>a.id === 823423));
+  // Find the comment with this ID
+  // delete the comment with the ID of 823423
+  const newComments = [...comments]
+  newComments.splice(comments.findIndex(a=>a.id === 823423),1)
+  console.log(`array if comment with the ID 823423 is deleted`, newComments);
+
 })();
